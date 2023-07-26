8bc75a98407cab14faf0751c8890cf4332901bce
Nic Vannetti <nic.vannetti@aalto.it>
2023-07-26 12:57:22 +0200

    added post fetch

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,30 @@
-function page() {
-    return <div>about</div>;
+async function page() {
+    async function getData() {
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        // The return value is *not* serialized
+        // You can return Date, Map, Set, etc.
+
+        // Recommendation: handle errors
+        if (!res.ok) {
+            // This will activate the closest `error.js` Error Boundary
+            throw new Error("Failed to fetch data");
+        }
+
+        return res.json();
+    }
+
+    const data = await getData();
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {data.map((post: any) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default page;
